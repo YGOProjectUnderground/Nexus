76942204e93fdd72aa9735ad7156e33d21e1caa3
DuDono <62557649+DuDono@users.noreply.github.com>
2025-01-30 00:04:50 +0000

    Change 1st effect because it was confusing (#7)

--- script/users/DuDono_004/c2004000006.lua
+++ script/users/DuDono_004/c2004000006.lua
@@ -7,14 +7,15 @@ function s.initial_effect(c)
   e1:SetType(EFFECT_TYPE_ACTIVATE)
   e1:SetCode(EVENT_FREE_CHAIN)
   c:RegisterEffect(e1)
-  -- lingering effects no more
-  local e2 = Effect.CreateEffect(c)
-  e2:SetDescription(aux.Stringid(id,0))
-  e2:SetType(EFFECT_TYPE_IGNITION)
-  e2:SetRange(LOCATION_SZONE)
-  e2:SetTarget(s.lintg)
-  e2:SetOperation(s.linop)
-  c:RegisterEffect(e2)
+  -- protect eepy girl
+  local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_IMMUNE_EFFECT)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetTargetRange(LOCATION_EXTRA,LOCATION_EXTRA)
+	e2:SetTarget(s.etarget)
+	e2:SetValue(s.efilter)
+	c:RegisterEffect(e2)
   -- search
   local e3 = Effect.CreateEffect(c)
   e3:SetDescription(aux.Stringid(id,1))
@@ -32,7 +33,7 @@ function s.initial_effect(c)
 	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e4:SetCode(EVENT_REMOVE)
 	e4:SetProperty(EFFECT_FLAG_DELAY)
-	e4:SetRange(LOCATION_MZONE)
+	e4:SetRange(LOCATION_SZONE)
 	e4:SetCountLimit(1,{id,0})
 	e4:SetCondition(s.bancon)
 	e4:SetTarget(s.bantg)
@@ -42,85 +43,16 @@ end
 s.listed_series={SET_NEMLERIA}
 s.listed_names={CARD_DREAMING_NEMLERIA}
 
+function s.etarget(e,c)
+	return c:IsFaceup() and c:IsCode(CARD_DREAMING_NEMLERIA)
+end
+function s.efilter(e,re)
+	return re:GetOwnerPlayer()~=e:GetHandlerPlayer()
+end
+
 function s.rmcfilter(c)
 	return c:IsFacedown() and c:IsAbleToRemoveAsCost(POS_FACEDOWN)
 end 
-function s.linfilter(c)
-  return c:IsAbleToRemoveAsCost(POS_FACEDOWN)
-end
-function s.lintg(e,tp,eg,ep,ev,re,r,rp,chk)
-  local desc = false
-  if Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode,CARD_DREAMING_NEMLERIA),tp,LOCATION_EXTRA,0,1,nil)
-    and Duel.IsExistingMatchingCard(s.rmcfilter,tp,LOCATION_EXTRA,0,3,nil) then
-    local slin = {Duel.GetPlayerEffect(tp)}
-    local olin = {Duel.GetPlayerEffect(1-tp)}
-    for i,te in ipairs(slin) do
-      local de = te:GetDescription()
-      if de > 0 then
-        desc = true
-      end
-    end
-    for i,te in ipairs(olin) do
-      local de = te:GetDescription()
-      if de > 0 then
-        desc = true
-      end
-    end
-  end
-  if chk==0 then return desc end
-  local g=Duel.SelectMatchingCard(tp,s.rmcfilter,tp,LOCATION_EXTRA,0,3,3,nil)
-  Duel.Remove(g,POS_FACEDOWN,REASON_COST)
-end
-function s.linop(e,tp,eg,ep,ev,re,r,rp)
-  local slin = {Duel.GetPlayerEffect(tp)}
-  local olin = {Duel.GetPlayerEffect(1-tp)}
-  local sdescs = {}
-  local odescs = {}
-  local cans = false
-  local cano = false
-  for i,te in ipairs(slin) do
-    local de = te:GetDescription()
-    if de > 0 then
-      sdescs[i] = de
-      cans = true
-    end
-  end
-  for i,te in ipairs(olin) do
-    local de = te:GetDescription()
-    if de > 0 then
-      odescs[i] = de
-      cano = true
-    end
-  end
-  if not (cans or cano) then return end
-  local op = Duel.SelectEffect(tp,{cans,aux.Stringid(id,2)},{cano,aux.Stringid(id,3)})
-  if op == 1 then
-    --delete slin[sop]
-    local sop = Duel.SelectOption(tp,sdescs)
-    local cnt = 0
-    for i,te in ipairs(slin) do
-      if te:GetDescription() > 0 then
-        if cnt == sop then
-          Effect.Reset(te,tp,1,0)
-        end
-        cnt = cnt + 1
-      end
-    end
-  else
-    --delete olin[sop]
-    local sop = Duel.SelectOption(tp,odescs)
-    local cnt = 0
-    for i,te in ipairs(olin) do
-      if te:GetDescription() > 0 then
-        if cnt == sop then
-          Effect.Reset(te,tp,1,0)
-        end
-        cnt = cnt + 1
-      end
-    end
-  end
-end
-
 function s.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.rmcfilter,tp,LOCATION_EXTRA,0,2,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
@@ -128,7 +60,7 @@ function s.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.Remove(g,POS_FACEDOWN,REASON_COST)
 end
 function s.thfilter(c)
-	return c:IsSetCard(SET_NEMLERIA) and c:IsAbleToHand()
+	return c:IsSetCard(SET_NEMLERIA) and c:IsAbleToHand() and c:IsMonster()
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end

ea9bc10619849527588417cf09382dfac718ba03
GenesicZyrael <69226206+GenesicZyrael@users.noreply.github.com>
2025-01-30 08:14:51 +0000

    Update: Gishki Photomirror (#6)
    * Update: Gishki Photomirror
    
    Code update on "Gishki Photomirror" for compatibility with EFFECT_EXTRA_RITUAL_LOCATION.
    
    * Update "Gishki Photomirror"
    
    ---------
    
    Co-authored-by: Lilac <107518574+Satellaa@users.noreply.github.com>

--- script/official/c9236985.lua
+++ script/official/c9236985.lua
@@ -4,83 +4,62 @@
 Duel.LoadScript("_load_.lua")
 local s,id=GetID()
 function s.initial_effect(c)
-	--Activate
-	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_FREE_CHAIN) 
-	e1:SetTarget(s.target)
-	e1:SetOperation(s.activate)
-	c:RegisterEffect(e1)
-	if not s.ritual_matching_function then
-		s.ritual_matching_function={}
-	end
-	s.ritual_matching_function[c]=aux.FilterEqualFunction(Card.IsSetCard,0x3a)
+    --Ritual Summon any "Gishki" Ritual Monster
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e1:SetType(EFFECT_TYPE_ACTIVATE)
+    e1:SetCode(EVENT_FREE_CHAIN)
+    e1:SetTarget(s.target)
+    e1:SetOperation(s.activate)
+    c:RegisterEffect(e1)
+    if not s.ritual_matching_function then
+        s.ritual_matching_function={}
+    end
+    s.ritual_matching_function[c]=aux.FilterEqualFunction(Card.IsSetCard,SET_GISHKI)
 end
-s.listed_series={0x3a}
-function s.filter(c,e,tp,lp)
-	if not c:IsRitualMonster() or not c:IsSetCard(0x3a) or not c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_RITUAL,tp,true,false) then
-		return false
+s.listed_series={SET_GISHKI}
+function s.ritualfilter(c,e,tp,lp)
+    if not c:IsRitualMonster() or not c:IsSetCard(SET_GISHKI) or not c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_RITUAL,tp,false,true) then return false end
+    if not c:IsLocation(LOCATION_HAND) then
+		local extra_loc_eff,used=Ritual.ExtraLocationOPTCheck(c,e:GetHandler(),tp)
+		if not extra_loc_eff or extra_loc_eff and used then return false end
+		if extra_loc_eff:GetProperty()&EFFECT_FLAG_GAIN_ONLY_ONE_PER_TURN>0 and Duel.HasFlagEffect(tp,EFFECT_FLAG_GAIN_ONLY_ONE_PER_TURN) then 
+			return false 
+		end
 	end
 	return lp>c:GetLevel()*500
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	local lp=Duel.GetLP(tp)
-	local c=e:GetHandler()
-	--- Custom ---
-	local location=LOCATION_HAND
-	local extra_loc=Duel.GetFlagEffectLabel(tp,CUSTOM_RITUAL_LOCATION)
-	local ec=Duel.IsExistingMatchingCard(s.filter,tp,location,0,1,nil,e,tp,lp)
-	local ec_extra
-	--------------
-	if chk==0 then
-		--- Custom ---
-		if Duel.GetFlagEffect(tp,CUSTOM_RITUAL_LOCATION)==1 and extra_loc and (location&extra_loc)==0 then
-			ec_extra=Duel.IsExistingMatchingCard(s.filter,tp,extra_loc,0,1,nil,e,tp,lp)
-			if ec_extra and ec then
-				location=location+extra_loc
-			elseif ec_extra and not ec then
-				location=extra_loc
-			end
-		else 
-			location=location
-		end
-		--------------
-		return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-			and Duel.IsExistingMatchingCard(s.filter,tp,location,0,1,nil,e,tp,lp)
-	end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,location)
+    if chk==0 then
+        local lp=Duel.GetLP(tp)
+        return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and Duel.IsExistingMatchingCard(s.ritualfilter,tp,LOCATION_HAND|LOCATION_NOTHAND,0,1,nil,e,tp,lp)
+    end
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	local lp=Duel.GetLP(tp)
-	--- Custom ---
-	local location=LOCATION_HAND
-	local extra_loc=Duel.GetFlagEffectLabel(tp,CUSTOM_RITUAL_LOCATION)
-	local ec=Duel.IsExistingMatchingCard(s.filter,tp,location,0,1,nil,e,tp,lp)
-	local ec_extra
-	if Duel.GetFlagEffect(tp,CUSTOM_RITUAL_LOCATION)==1 and extra_loc and (location&extra_loc)==0 then
-		ec_extra=Duel.IsExistingMatchingCard(s.filter,tp,extra_loc,0,1,nil,e,tp,lp)
-		if ec_extra and ec then
-			if Duel.SelectYesNo(tp,aux.Stringid(CUSTOM_RITUAL_LOCATION,1)) then
-				Duel.RegisterFlagEffect(tp,CUSTOM_RITUAL_LOCATION,RESET_PHASE+PHASE_END,0,1,extra_loc)
-				location = extra_loc
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	-- custom --
+	local tg=Duel.GetMatchingGroup(aux.NecroValleyFilter(s.ritualfilter),tp,LOCATION_HAND|LOCATION_NOTHAND,0,nil,e,tp,lp)
+	local tc=tg:Select(tp,1,1,nil):GetFirst()
+	if not tc then return end
+	local extra_loc_eff=Ritual.GetExtraLocationEffect(tc,e:GetHandler())
+	if extra_loc_eff and extra_loc_eff:CheckCountLimit(tp) then
+		local extra_loc=extra_loc_eff:GetTargetRange()
+		if extra_loc_eff:GetType()&EFFECT_TYPE_SINGLE>0 or extra_loc and tc:IsLocation(extra_loc) then
+			extra_loc_eff:UseCountLimit(tp)
+			if extra_loc_eff:GetProperty()&EFFECT_FLAG_GAIN_ONLY_ONE_PER_TURN>0 then
+				Duel.RegisterFlagEffect(tp,EFFECT_FLAG_GAIN_ONLY_ONE_PER_TURN,RESET_PHASE|PHASE_END,0,1)
 			end
-		elseif ec_extra and not ec then
-			Duel.RegisterFlagEffect(tp,CUSTOM_RITUAL_LOCATION,RESET_PHASE+PHASE_END,0,1,extra_loc)
-			location = extra_loc
 		end
 	end
-	--------------
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local tg=Duel.SelectMatchingCard(tp,s.filter,tp,location,0,1,1,nil,e,tp,lp)
-	local tc=tg:GetFirst()
-	if tc then
-		mustpay=true
-		Duel.PayLPCost(tp,tc:GetLevel()*500)
-		mustpay=false
-		tc:SetMaterial(nil)
-		Duel.SpecialSummon(tc,SUMMON_TYPE_RITUAL,tp,tp,true,false,POS_FACEUP)
-		tc:CompleteProcedure()
-	end
+	--
+	mustpay=true
+	Duel.PayLPCost(tp,tc:GetLevel()*500)
+	mustpay=false
+	tc:SetMaterial(nil)
+	Duel.SpecialSummon(tc,SUMMON_TYPE_RITUAL,tp,tp,false,true,POS_FACEUP)
+	tc:CompleteProcedure()
 end

85c6dc8dd3efc03975dd15297503d90abb8d53e0
DuDono <62557649+DuDono@users.noreply.github.com>
2025-02-07 03:22:32 +0000

    Bug fixed : Soleil no longer sends a Beast as cost. (#15)
    * Bug fix : Soleil no longer sends a Beast as cost.
    
    * Update c2004000002.lua
    
    - change `REASON_COST` to `REASON_EFFECT`
    - add check that the sent card is in GY
    
    ---------
    
    Co-authored-by: Lilac <107518574+Satellaa@users.noreply.github.com>

--- script/users/DuDono_004/c2004000002.lua
+++ script/users/DuDono_004/c2004000002.lua
@@ -17,7 +17,6 @@ function s.initial_effect(c)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetCountLimit(1,id)
-	e2:SetCost(s.spdkcost)
 	e2:SetTarget(s.spdktg)
 	e2:SetOperation(s.spdkop)
 	c:RegisterEffect(e2)
@@ -66,25 +65,25 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 function s.tgcfilter(c)
-	return c:IsLevel(10) and c:IsRace(RACE_BEAST) and (c:IsFaceup() or c:IsLocation(LOCATION_HAND)) and c:IsAbleToGraveAsCost()
-end
-function s.spdkcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
-	if chk==0 then return Duel.IsExistingMatchingCard(s.tgcfilter,tp,LOCATION_HAND|LOCATION_MZONE,0,1,c) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g=Duel.SelectMatchingCard(tp,s.tgcfilter,tp,LOCATION_HAND|LOCATION_MZONE,0,1,1,c)
-	Duel.SendtoGrave(g,REASON_COST)
+	return c:IsLevel(10) and c:IsRace(RACE_BEAST) and (c:IsFaceup() or c:IsLocation(LOCATION_HAND)) and c:IsAbleToGrave()
 end
 function s.spdkfilter(c,e,tp)
 	return c:IsSetCard(SET_NEMLERIA) and c:IsMonster() and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.spdktg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.spdkfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.spdkfilter,tp,LOCATION_DECK,0,1,nil,e,tp) and
+		Duel.IsExistingMatchingCard(s.tgcfilter,tp,LOCATION_HAND|LOCATION_MZONE,0,1,c) end
 end
 function s.spdkop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
-	local g=Duel.SelectMatchingCard(tp,s.spdkfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
-	if #g>0 then
-		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	local c=e:GetHandler()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,s.tgcfilter,tp,LOCATION_HAND|LOCATION_MZONE,0,1,1,c)
+	if #g==0 then return end
+	if Duel.SendtoGrave(g,REASON_EFFECT)>0 and g:GetFirst():IsLocation(LOCATION_GRAVE) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local sg=Duel.SelectMatchingCard(tp,s.spdkfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+		if #sg>0 then
+			Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
+		end
 	end
 end
